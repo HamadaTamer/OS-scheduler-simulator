411b9609b9225ddc4286af80033eab4b6e9ae4e4
HamadaTamer <ahmedtamer930@gmail.com>
2025-04-17 04:27:37 +0300

    added the creation of the PCB

--- main.c
+++ main.c
@@ -41,12 +41,25 @@
 
 int MemorySize = 3;
 
+static int PCBID = 0; // a global counter of Process IDs in order to be tracked globally in other words ID to be used by next process initiated   
+
+struct MemoryWord *Memory_start_location;
+
+// structs defining the memoryWord layout
+
+typedef enum{
+    NEW,
+    READY, 
+    RUNNING,
+    WAITING,
+    TERMINATED
+} process_state;
+
 typedef enum {
     EXTRA_NONE,
     EXTRA_INT,
     EXTRA_STRING
 } variable_type_t;
-
 
 
 // a union is a structure that can hold either of 2 values and not both
@@ -68,7 +81,7 @@ struct MemoryWord * createMemory(){
     
     // creating program memory
     struct MemoryWord *arr = calloc(60, sizeof *arr);
-
+    Memory_start_location = arr;
     if (!arr){
         perror("failed to create memory ");
         EXIT_FAILURE;
@@ -136,6 +149,52 @@ void parseProgram(char *filename, struct MemoryWord *memory) {
 }
 
 
+// creating the PCB
+
+void createPCB(struct MemoryWord *memory, int priority){
+
+    /*
+    PCB is made up of 5 consecutive words for the following attributes:
+        1.Process ID (depends on the global counter)
+        2. Process state (states are: new, ready, running, waiting (blocked), terminated )
+        3. current priority (initialzed with supplied memory address)
+        4. Program counter (initialized with supplied memory address )
+        5. Memory boundaries // fixed at supplied memory location for start, and supplied mem location + number of instructions + 1 (EOI) + 5 (PCB) + (number of variables)
+    */
+    
+
+    int i = 0;
+
+    // finding EOI
+    while (strcmp(memory[i].identifier, "EOI") != 0) i++;
+    i++; // move to start of PCB area
+
+    // Process ID
+    strcpy(memory[i].identifier, "ID");
+    sprintf(memory[i].arg1, "%d", PCBID++);
+    
+    // Process State
+    i++;
+    strcpy(memory[i].identifier, "State");
+    sprintf(memory[i].arg1, "%d", NEW);
+    
+    // Priority
+    i++;
+    strcpy(memory[i].identifier, "Current_priority");
+    sprintf(memory[i].arg1, "%d", priority);
+    
+    // Program Counter
+    i++;
+    strcpy(memory[i].identifier, "Program_counter");
+    sprintf(memory[i].arg1, "%d", 0);
+    
+    // Memory Boundaries
+    i++;
+    strcpy(memory[i].identifier, "Memory_Bounds");
+    sprintf(memory[i].arg1, "%d",memory - Memory_start_location );           // Lower bound 
+    sprintf(memory[i].arg2, "%d", memory + i + 3 - Memory_start_location);       // Upper bound
+}
+
 
 int main() {
     // Step 1: Create the memory
@@ -151,10 +210,45 @@ int main() {
 
     // Step 3: Print out memory contents to verify
     printf("Parsed Memory Contents:\n");
-    for (int i = 0; i < 60; i++) {
+    for (int i = 0; i < 20; i++) {
         //if (strlen(memory[i].identifier) == 0) break; // Stop at empty slot
         printf("Memory[%d]: identifier = %s, arg1 = %s, arg2 = %s\n", i, memory[i].identifier, memory[i].arg1, memory[i].arg2);
     }
+
+   
+        // Step 3: Create the PCB
+        printf("Creating PCB for the parsed program...\n");
+        int testPriority = 2;
+        createPCB(memory, testPriority);
+    
+        // Step 4: Print out the memory contents to verify
+        printf("\n===== Memory Dump =====\n");
+        for (int i = 0; i < 60; i++) {
+            // Only print if something is stored
+            if (strlen(memory[i].identifier) == 0) continue;
+     
+            printf("Memory[%02d]: ", i);
+    
+            // Label the section
+            if (strcmp(memory[i].identifier, "EOI") == 0) {
+                printf("== End of Instructions ==\n");
+                continue;
+            } else if (strcmp(memory[i].identifier, "ID") == 0) {
+                printf("[PCB] %s = %s\n", memory[i].identifier, memory[i].arg1);
+            } else if (strcmp(memory[i].identifier, "State") == 0 ||
+                       strcmp(memory[i].identifier, "Current_priority") == 0 ||
+                       strcmp(memory[i].identifier, "Program_counter") == 0) {
+                printf("[PCB] %s = %s\n", memory[i].identifier, memory[i].arg1);
+            } else if (strcmp(memory[i].identifier, "Memory_Bounds") == 0) {
+                printf("[PCB] %s = (%s -> %s)\n", memory[i].identifier, memory[i].arg1, memory[i].arg2);
+            } else {
+                // Default: print instruction
+                printf("[Instr] %s %s %s\n", memory[i].identifier, memory[i].arg1, memory[i].arg2);
+            }
+        }
+    
+     
+    
 
     // Step 4: Cleanup
     free(memory);
